02c83795c169272473eb0a62fa430153fc2f1563
[Brody Van Eperen] <bvaneperen@hbs.net>
2024-05-31 11:13:18 -0500

    view components

--- Personal Portfolio/obj/Debug/net8.0/Personal Portfolio.AssemblyInfo.cs
+++ Personal Portfolio/obj/Debug/net8.0/Personal Portfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Personal Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e93f73562b7e384cfd833b8468063ba4f1eadc3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f683b25abd7b373ebf1fedd4bdf1ec4c1f84154")]
 [assembly: System.Reflection.AssemblyProductAttribute("Personal Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Personal Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
